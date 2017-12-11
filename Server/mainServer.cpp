b298fde0bc6e7dd83bd477a99ed4f8d5b59db380
omer <omer2wolf@gmail.com>
2017-12-11 03:53:04 +0200

    Make the port/ip file Update the server class to read from file port/ip

--- Server/mainServer.cpp
+++ Server/mainServer.cpp
@@ -5,10 +5,24 @@
 #include "Server.h"
 #include <iostream>
 #include <stdlib.h>
+#include <fstream>
 using namespace std;
 
+
 int main() {
+    ifstream myInfo;
+    int port;
+    myInfo.open("myInfo");
+    if(myInfo.is_open()){
+        myInfo >> port;
+    }else {
+        cout<<"Unable to open the server/ip file"<<endl;
+    }
+    Server server(port);
+=======
+int mainServer() {
     Server server(8000);
+>>>>>>> origin/master:Server/mainServer.cpp
     try {
         server.start();
     } catch  (const char *msg){
