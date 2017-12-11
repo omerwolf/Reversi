7bb465336575355ab911a36fa855005649e3c165
omer <omer2wolf@gmail.com>
2017-12-12 00:02:52 +0200

    Make the port/ip file Update the server class to read from file port/ip

--- src/Server/Server.h
+++ src/Server/Server.h
@@ -7,12 +7,33 @@
 
 class Server {
 private:
+
     int port, serverSocket;
     void handleClient(int clientSocket1, int clientSocket2);
 
 public:
+    /***********************************************************************
+    * function name: Server (con's)
+    * The input:  int
+    * The Output: No Output
+    * The Function operation: build the server from port and socket.   											   *
+    ***********************************************************************/
     Server(int port);
+    /***********************************************************************
+    * function name: start        							   *
+    * The input: No Input		  										   *
+    * The Output: No Output											       *
+    * The Function operation: initialize the server, define 2 clientSocket
+    * and listen the sockets. after accepting both clientSockets the function
+    * handle them.
+    ***********************************************************************/
     void start();
+    /***********************************************************************
+    * function name: stop        							   *
+    * The input: No Input		  										   *
+    * The Output: No Output											       *
+    * The Function operation: shut down the socket
+    ***********************************************************************/
     void stop();
 
 };
