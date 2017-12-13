2a7a948f22c1d8a2bd9373e2d942d18f37fe3798
yoel <yjasner@gmail.com>
2017-12-13 16:29:25 +0200

    Notes

--- src/Server/Server.h
+++ src/Server/Server.h
@@ -13,26 +13,26 @@ private:
 
 public:
     /***********************************************************************
-    * function name: Server (con's)
-    * The input:  int
-    * The Output: No Output
-    * The Function operation: build the server from port and socket.   											   *
+    * function name: Server (con's)                                        *
+    * The input:  int                                                      *
+    * The Output: No Output                                                *
+    * The Function operation: build the server from port and socket.       *									   *
     ***********************************************************************/
     Server(int port);
     /***********************************************************************
-    * function name: start        							   *
+    * function name: start        		            					   *
     * The input: No Input		  										   *
     * The Output: No Output											       *
     * The Function operation: initialize the server, define 2 clientSocket
     * and listen the sockets. after accepting both clientSockets the function
-    * handle them.
+    * handle them.                                                         *
     ***********************************************************************/
     void start();
     /***********************************************************************
-    * function name: stop        							   *
+    * function name: stop        							               *
     * The input: No Input		  										   *
     * The Output: No Output											       *
-    * The Function operation: shut down the socket
+    * The Function operation: shut down the socket                         *
     ***********************************************************************/
     void stop();
 
