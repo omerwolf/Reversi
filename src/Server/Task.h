d5c270314df74bda32966857bfcf442796de693e
omer <omer2wolf@gmail.com>
2018-01-26 13:16:42 +0200

    write comments

--- src/Server/Task.h
+++ src/Server/Task.h
@@ -7,12 +7,35 @@
 
 class Task {
 public:
+    /***********************************************************************
+    * function name: Task (default con's)                                        *
+    * The input:  No input                                                      *
+    * The Output: No Output                                                *
+    * The Function operation: build the Task from func and arg.       *									   *
+    ***********************************************************************/
     Task(void* (*func)(void* arg), void*arg):func(func), arg(arg) {}
+    /***********************************************************************
+    * function name: excute                                       *
+    * The input:  No input                                                      *
+    * The Output: No Output                                                *
+    * The Function operation: activate the arg as input for func       *									   *
+    ***********************************************************************/
     void excute(){
         func (arg);
     }
+    /***********************************************************************
+    * function name: Task (d'tor)                                  *
+    * The input: no input		 										     *
+    * The Output: no output										         *                         *
+    ***********************************************************************/
     virtual ~Task(){}
 private:
+    /***********************************************************************
+    * function name: *func                                   *
+    * The input: no input		 										     *
+    * The Output: no output
+    * The Function operation: The function that the task needs to execute                       *
+    ***********************************************************************/
     void* (*func)(void* arg);
     void* arg;
 };
--- src/Server/ThreadPool.h
+++ src/Server/ThreadPool.h
@@ -11,19 +11,54 @@ using namespace std;
 
 class ThreadPool {
 public:
+    /***********************************************************************
+    * function name: ThreadPool                                       *
+    * The input:  int                                                      *
+    * The Output: No Output                                                *
+    * The Function operation: build the ThreadPool according the threadNum.       *									   *
+    ***********************************************************************/
     ThreadPool (int threadNum);
+    /***********************************************************************
+    * function name: addTask                                       *
+    * The input:  Task*                                                      *
+    * The Output: No Output                                                *
+    * The Function operation: push task to the tasksQueue.       *									   *
+    ***********************************************************************/
     void addTask(Task* task);
+    /***********************************************************************
+    * function name: terminate                                       *
+    * The input:  No input                                                      *
+    * The Output: No Output                                                *
+    * The Function operation: cancel the mutex and kill the TreadPoll.       *									   *
+    ***********************************************************************/
     void terminate();
+    /***********************************************************************
+    * function name: ThreadPool (d'tor)                                  *
+    * The input: no input		 										     *
+    * The Output: no output										         *                         *
+    ***********************************************************************/
     virtual ~ThreadPool();
 
-private:
+    private:
     queue <Task*> tasksQueue;
     pthread_t* threads;
-    void executeTask();
     bool stopped;
     pthread_mutex_t lock;
+    /***********************************************************************
+    * function name: executeTask                                       *
+    * The input:  No input                                                      *
+    * The Output: No Output                                                *
+    * The Function operation: The function that each thread runs.       *									   *
+    ***********************************************************************/
+    void executeTask();
+    /***********************************************************************
+    * function name: excute                                       *
+    * The input:  void*                                                      *
+    * The Output: No Output                                                *
+    * The Function operation: A static wrapper that calls executeTask()        *									   *
+    ***********************************************************************/
     static void* excute(void* arg);
-};
+    };
 
 
 #endif //EX3_THREADPOOL_H
